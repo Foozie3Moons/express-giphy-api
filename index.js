063d0e4d37d970be07e1a22bb756c8e037a568bb
foozie3moons <matthew.bell.290@gmail.com>
2017-09-21 10:14:59 -0700

    used template engine to generate images

--- index.js
+++ index.js
@@ -10,24 +10,47 @@ app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
   var body = {};
-  res.render('index', {data: body});
+  res.render('../static/index.pug');
+});
+
+app.get('/search/many/:query', function(req, res) {
+  var url = 'http://api.giphy.com/v1/gifs/search?'
+  request({
+    url: url,
+    qs: {
+      limit: 1000,
+      api_key: 'UGAbbHcK7BJHUP9pGS8EVxwm992Fe9tz',
+      q: req.params.query,
+      rating: 'g'
+    }
+  }, function(error, response, body) {
+    body = JSON.parse(body);
+    var data = body.data;
+    resData = [];
+    for (var i = 0; i < data.length; i++) {
+      index = Math.floor(Math.random() * data.length);
+      resData.push(data[index]);
+    }
+    res.render('index', {data: resData})
+  });
 });
 
 app.get('/search/one/:query', function(req, res) {
   var url = 'http://api.giphy.com/v1/gifs/search?'
   request({
-    url: url
+    url: url,
     qs: {
-      limit: 20,
+      limit: 100,
       api_key: 'UGAbbHcK7BJHUP9pGS8EVxwm992Fe9tz',
       q: req.params.query,
-      rating: g
+      rating: 'g'
     }
   }, function(error, response, body) {
     body = JSON.parse(body);
     var data = body.data;
     var index = Math.floor(Math.random() * data.length);
-    res.render('index', data[index].images.downsize_medium.url)
+    data = data[index];
+    res.render('index', {data: [data]})
   });
 });
 
